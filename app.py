b09113d0f1c1fbc5f9a9c5d24c9fb09342f8a9bd
arisia <starlights2283@gmail.com>
2023-11-02 13:48:31 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+i = 0
+while(True):
+    print("이야아")
+    if(i > 100000):
+        break
+    i += 1
---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+
+def loader(get_model):
+    getmodel_text = get_model + ".HDF5"
+    print(getmodel_text)
+    
+    
