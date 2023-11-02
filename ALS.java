75c6ac1462b2772c5ff62055fc3af9d867143588
arisia <starlights2283@gmail.com>
2023-11-02 14:03:58 +0900

    Update files

---
+++ ALS.java
@@ -0,0 +1,8 @@
+public class ALS {
+    public static void main(String[] args){
+        int[] score = {10, 20, 30, 40, 50, 60, 70};
+        for(int i=0; i < score.length; i++){
+            System.out.println(score[i]);
+        }
+    }
+}
